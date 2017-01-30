348e1fc1248671e1857aef969958c54c294e11f3
Lucas Bourneuf <lucas.bourneuf@laposte.net>
2017-01-30 11:50:22 +0100

    vizumark: use boxes module to extend markdown behavior + expect fa path in cli

--- vizumark.py
+++ vizumark.py
@@ -4,17 +4,27 @@ import sys
 import webbrowser
 from markdown import markdown
 
+from boxes import BoxExtension
+
+
+def header(css:str=None) -> str:
+    if css:
+        return '<head> <link rel="stylesheet" href="{}"> </head>'.format(css)
+    return ''
 
 
 if __name__ == "__main__":
-    if len(sys.argv) == 1:
-        print('usage: <markdown file>')
+    if len(sys.argv) != 3:
+        print('usage: <markdown file> <path to font awesome css file>')
         exit()
 
     markdown_file = sys.argv[1]
+    path_to_fa = sys.argv[2]
+    assert os.path.exists(path_to_fa)
     html_file = os.path.splitext(markdown_file)[0] + '.html'
     with open(markdown_file) as fd:
-        html = markdown(fd.read())
+        html = markdown(fd.read(), extensions=[BoxExtension()])
     with open(html_file, 'w') as fd:
+        fd.write(header(css=path_to_fa))
         fd.write(html)
     webbrowser.open(html_file)
